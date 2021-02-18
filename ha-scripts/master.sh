04d8310eafedf2db3f3646732a15e236adaef707
Jason Barbee <jason.barbee@gmail.com>
2021-02-18 18:11:18 -0500

    remove cidr

--- ha-scripts/master.sh
+++ ha-scripts/master.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 export NODES=3
+export cpus_per_api=1
+# These are the two primary Virtual IPs for the CURRI API
 export primary_ip="192.168.0.235"
 export secondary_ip="192.168.0.236"
-export cluster_start="192.168.0.237"
-export cluster_end="192.168.0.238"
-export cluster_cidr="192.168.100.0/24"
-export service_cidr="192.168.101.0/24"
-export dns_service="192.168.101.10"
+# This is a pool of IPs for Traefik Controller
+export cluster_ip="192.168.0.237"
+# You might need to change the internal cluster IPs so they don't conflict with your external resources. 
+# export cluster_cidr="192.168.100.0/24"
+# export service_cidr="192.168.101.0/24"
+# export dns_service="192.168.101.10"
 # Note: This sets the PostreSQL master user. It should be changed for production use.
 export DB_PASSWORD="calltelemetry"
 export INSTALL_K3S_CHANNEL=stable
@@ -14,7 +17,8 @@ export K3S_KUBECONFIG_MODE="0644"
 # Note: This token is a preshared-key shared between the cluster. It should be changed for production use.
 export K3S_TOKEN="calltelemetry"
 
-curl -sfL https://get.k3s.io | sh -s server --cluster-init --cluster-cidr $cluster_cidr --service-cidr $service_cidr --cluster-dns $dns_service --no-deploy traefik --disable servicelb
+# Nothing below this needs to be edited
+curl -sfL https://get.k3s.io | sh -s server --cluster-init --no-deploy traefik --disable servicelb
 mkdir -p ~/.kube
 sudo cat /etc/rancher/k3s/k3s.yaml > ~/.kube/config
 
@@ -54,10 +58,10 @@ sudo cp ~/.pgo/pgo/pgo /usr/local/bin
 /snap/bin/kubectl port-forward -n pgo svc/postgres-operator 8443:8443 &
 sleep 5
 echo "Installing Postgres Cluster + Replicas, will take a moment to complete"
-/usr/local/bin/pgo create cluster -n pgo ctsql -d calltelemetry_prod --replica-count $NODES --password-superuser=$DB_PASSWORD
+/usr/local/bin/pgo create cluster -n pgo ctsql -d calltelemetry_prod --password-superuser="calltelemetry"
 
 echo "Starting CallTelemetry deployment via Helm Chart"
-cat <<EOF > ~/ct_values.yaml
+cat <<EOF > ./custom_values.yaml
 # ct_values.yaml
 db_password: $DB_PASSWORD
 web_replicas: $NODES
@@ -65,8 +69,12 @@ primary_ip: $primary_ip
 secondary_ip: $secondary_ip
 cluster_ip_start: $cluster_start
 cluster_ip_end: $cluster_end
+web:
+  cpus: $cpus_per_api
+  imagePullPolicy: IfNotPresent
+  replicas: $NODES
 EOF
 /snap/bin/helm repo add ct_charts https://storage.googleapis.com/ct_charts/
 /snap/bin/helm repo update
-/snap/bin/helm install ct ct_charts/stable-ha -f ~/ct_values.yaml
+/snap/bin/helm install ct ct_charts/stable-ha -f ./custom_values.yaml
 echo "CallTelemetry Deployment complete"
--- ha-scripts/secondary.sh
+++ ha-scripts/secondary.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
-export primary_ip="192.168.123.135"
-export K3S_URL="https://$primary_ip:6443"
-export INSTALL_K3S_CHANNEL=stable
-export K3S_KUBECONFIG_MODE="0644"
+# Edit this to be the primary node / any master node.
+export primary_ip="192.168.0.148"
+# You might need to change the internal cluster IPs so they don't conflict with your external resources. 
+export cluster_cidr="192.168.100.0/24"
+export service_cidr="192.168.101.0/24"
+export dns_service="192.168.101.10"
 # Note: This token is a preshared-key shared between the cluster. It should be changed for production use.
 export K3S_TOKEN="calltelemetry"
 
-# Install Kubectl and Helm via SNAPS
-sudo yum install -y wget epel-release snapd
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
-sudo snap wait system seed.loaded
-sudo systemctl restart snapd.seeded.service
-sudo snap install kubectl --classic
-sudo snap install helm --classic
-
-# Install K9s Kubernetes Management tool
-echo "Installing k9s toolkit - https://github.com/derailed/k9s/"
-wget https://github.com/derailed/k9s/releases/download/v0.24.2/k9s_Linux_x86_64.tar.gz
-tar -xzf k9s_Linux_x86_64.tar.gz
-sudo mv k9s /usr/local/bin
-mkdir ~/.k9s
-rm -rf k9s*
+# Nothing below this needs to be edited
+export K3S_URL="https://$primary_ip:6443"
+export INSTALL_K3S_CHANNEL=stable
+export K3S_KUBECONFIG_MODE="0644"
 
 # Install Master
-curl -sfL https://get.k3s.io | sh -s server --no-deploy traefik --disable servicelb
+curl -sfL https://get.k3s.io | sh -s server  --no-deploy traefik --disable servicelb
 mkdir -p ~/.kube
 sudo cat /etc/rancher/k3s/k3s.yaml > ~/.kube/config
+
+export PGOUSER="${HOME?}/.pgo/pgo/pgouser"
+export PGO_CA_CERT="${HOME?}/.pgo/pgo/client.crt"
+export PGO_CLIENT_CERT="${HOME?}/.pgo/pgo/client.crt"
+export PGO_CLIENT_KEY="${HOME?}/.pgo/pgo/client.key"
+export PGO_APISERVER_URL='https://127.0.0.1:8443'
+export PGO_NAMESPACE=pg
+
+echo "Installing CrunchData PGO Client"
+# wget https://raw.githubusercontent.com/CrunchyData/postgres-operator/v4.5.1/installers/kubectl/client-setup.sh
+curl -s https://raw.githubusercontent.com/CrunchyData/postgres-operator/v4.5.1/installers/kubectl/client-setup.sh > ~/client-setup.sh
+chmod +x ~/client-setup.sh
+~/client-setup.sh
+ls -la ~/.pgo/pgo
+# ls -la /home/calltelemetry/.pgo/pgo
+sudo cp ~/.pgo/pgo/pgo /usr/local/bin
+/snap/bin/kubectl port-forward -n pgo svc/postgres-operator 8443:8443 &
+
+echo "Scaling SQL Replica to this node"
+/usr/local/bin/pgo scale -n pgo ctsql --no-prompt
