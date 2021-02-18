ee57dbb0664d05ac070af3a285719e431c4fd9e9
CallTelemetry <calltelemetry@calltelemetry.com>
2021-02-18 18:11:18 -0500

    remove cidr

--- ha-scripts/centos_prep.sh
+++ ha-scripts/centos_prep.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Disable Firewall and selinux
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo setenforce permissive
+
 # Install Kubectl and Helm via SNAPS
 sudo yum install -y wget epel-release 
 sudo yum install -y snapd
@@ -16,3 +21,11 @@ tar -xzf k9s_Linux_x86_64.tar.gz
 sudo mv k9s /usr/local/bin
 mkdir ~/.k9s
 rm -rf k9s*
+
+# Install GIT
+sudo dnf install -y git
+
+# Clone repo
+git clone https://github.com/calltelemetry/calltelemetry.git
+
+echo "cd calltelemetry/ha-scripts, and run master.sh or secondary.sh"
