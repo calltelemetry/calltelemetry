760e1cf78b06ad92a5cdb18e30baa77cb2daedda
CallTelemetry <calltelemetry@calltelemetry.com>
2021-02-18 19:22:51 -0500

    remove cidr

--- ha-scripts/master.sh
+++ ha-scripts/master.sh
@@ -6,10 +6,6 @@ export primary_ip="192.168.0.235"
 export secondary_ip="192.168.0.236"
 # This is a pool of IPs for Traefik Controller
 export cluster_ip="192.168.0.237"
-# You might need to change the internal cluster IPs so they don't conflict with your external resources. 
-# export cluster_cidr="192.168.100.0/24"
-# export service_cidr="192.168.101.0/24"
-# export dns_service="192.168.101.10"
 # Note: This sets the PostreSQL master user. It should be changed for production use.
 export DB_PASSWORD="calltelemetry"
 export INSTALL_K3S_CHANNEL=stable
@@ -44,16 +40,13 @@ EOF
 source ~/.bashrc
 
 echo "Installing PGO Operator - Wait for approximately 3 minutes for it to complete"
-echo "Run kubectl get pod -A to see if it is running before continuing."
 sleep 180
 
 echo "Installing CrunchData PGO Client"
-# wget https://raw.githubusercontent.com/CrunchyData/postgres-operator/v4.5.1/installers/kubectl/client-setup.sh
 curl -s https://raw.githubusercontent.com/CrunchyData/postgres-operator/v4.5.1/installers/kubectl/client-setup.sh > ~/client-setup.sh
 chmod +x ~/client-setup.sh
 ~/client-setup.sh
 ls -la ~/.pgo/pgo
-# ls -la /home/calltelemetry/.pgo/pgo
 sudo cp ~/.pgo/pgo/pgo /usr/local/bin
 /snap/bin/kubectl port-forward -n pgo svc/postgres-operator 8443:8443 &
 sleep 5
