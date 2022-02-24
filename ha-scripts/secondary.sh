9521e9524954525e406fd7f81234ade9a80e7702
CallTelemetry <calltelemetry@calltelemetry.com>
2022-02-24 00:41:27 -0500

    bump versions & Lock down rpcservice.

--- ha-scripts/secondary.sh
+++ ha-scripts/secondary.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Edit this to be the primary node / any master node.
-export primary_ip="192.168.0.148"
+export primary_ip="192.168.0.128"
 #his token is a preshared-key shared between the cluster. It should be changed for production use.
 export K3S_TOKEN="calltelemetry"
 
@@ -8,6 +8,7 @@ export K3S_TOKEN="calltelemetry"
 export K3S_URL="https://$primary_ip:6443"
 export INSTALL_K3S_CHANNEL=stable
 export K3S_KUBECONFIG_MODE="0644"
+export INSTALL_K3S_VERSION="v1.20.2+k3s1"
 
 # Install Master
 curl -sfL https://get.k3s.io | sh -s server  --no-deploy traefik --disable servicelb
@@ -22,8 +23,7 @@ export PGO_APISERVER_URL='https://127.0.0.1:8443'
 export PGO_NAMESPACE=pgo
 
 echo "Installing CrunchData PGO Client"
-# wget https://raw.githubusercontent.com/CrunchyData/postgres-operator/v4.5.1/installers/kubectl/client-setup.sh
-curl -s https://raw.githubusercontent.com/CrunchyData/postgres-operator/v4.5.1/installers/kubectl/client-setup.sh > ~/client-setup.sh
+curl -s https://raw.githubusercontent.com/CrunchyData/postgres-operator/v4.6.5/installers/kubectl/client-setup.sh > ~/client-setup.sh
 chmod +x ~/client-setup.sh
 ~/client-setup.sh
 ls -la ~/.pgo/pgo
