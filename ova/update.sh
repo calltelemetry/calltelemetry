9e2211bbda411ccd969ad929045bace02cbb0ca1
calltelemetry-jason <jason@calltelemetry.com>
2022-10-14 09:30:50 -0400

    update fix

--- ova/update.sh
+++ ova/update.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # Backup Docker config
-cp docker-compose.yml old-docker-compose.yml
+mv docker-compose.yml old-docker-compose.yml
 # Download new docker compose file
-wget https://storage.googleapis.com/ct_ovas/docker-compose.yml -o docker-compose.yml
+wget https://storage.googleapis.com/ct_ovas/docker-compose.yml -O docker-compose.yml
 # Pull new image
 docker-compose pull
 # Restart docker
@@ -11,6 +11,10 @@ systemctl restart docker-compose-app.service
 rm backup.sh
 wget https://raw.githubusercontent.com/calltelemetry/calltelemetry/master/ova/backup.sh -O backup.sh
 chmod +x backup.sh
+# Fix for appliance permissions on pre-0.6.8
+sudo chown -R calltelemetry /home/calltelemetry/backups
+chmod +x /home/calltelemetry/backup.sh
+chown calltelemetry /home/calltelemetry/backup.sh
 rm update.sh
 wget https://raw.githubusercontent.com/calltelemetry/calltelemetry/master/ova/update.sh -O update.sh
 chmod +x update.sh
