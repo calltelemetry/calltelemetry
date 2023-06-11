edf760b1d0a3ba999ac1198bec60f2ba54ab4264
CallTelemetry <calltelemetry@calltelemetry.com>
2023-06-11 14:07:32 -0400

    update script for 073

---
+++ ova/update-073.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# Backup Docker config, if it exists.
+mv docker-compose.yml old-docker-compose.yml
+# Download new docker compose file
+wget https://storage.googleapis.com/ct_ovas/docker-compose-073.yml -O docker-compose.yml
+# Pull new image
+docker-compose pull
+# Restart docker
+systemctl restart docker-compose-app.service
+# Fresh Install & Fix for appliance permissions on pre-0.6.8
+mkdir -p backups
+sudo chown -R calltelemetry backups
+# Refresh OVA scripts
+rm backup.sh
+wget https://raw.githubusercontent.com/calltelemetry/calltelemetry/master/ova/backup.sh -O backup.sh
+chmod +x backup.sh
+chmod +x backup.sh
+chown calltelemetry backup.sh
+rm update.sh
+wget https://raw.githubusercontent.com/calltelemetry/calltelemetry/master/ova/update.sh -O update.sh
+chmod +x update.sh
+rm reset.sh
+wget https://raw.githubusercontent.com/calltelemetry/calltelemetry/master/ova/reset.sh -O reset.sh
+chmod +x reset.sh
+rm -rf /home/calltelemetry/.ssh/authorized_keys
