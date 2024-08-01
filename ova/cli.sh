933178aa5363d8905cee9854124359c5e09b393f
CallTelemetry <calltelemetry@calltelemetry.com>
2024-08-01 09:45:08 -0500

    update cli tool to set logging and rollback

--- ova/cli.sh
+++ ova/cli.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # Directory for storing backups and other directories to be cleared
 BACKUP_DIR="/home/calltelemetry/backups"
@@ -7,7 +7,6 @@ SFTP_DIR="sftp/*"
 POSTGRES_DATA_DIR="postgres-data"
 # Original and backup docker-compose files
 ORIGINAL_FILE="docker-compose.yml"
-BACKUP_FILE="$BACKUP_DIR/old-docker-compose.yml"
 TEMP_FILE="temp-docker-compose.yml"
 
 # Ensure necessary directories exist and have correct permissions
@@ -18,23 +17,30 @@ sudo chown -R calltelemetry "$BACKUP_FOLDER_PATH"
 
 # Function to display help
 show_help() {
-  echo "Usage: script_name.sh [option]"
+  echo "Usage: script_name.sh [option] [parameter]"
   echo
   echo "Options:"
-  echo "  --help            Show this help message and exit."
-  echo "  update            Update the docker-compose configuration and restart the service."
-  echo "  rollback          Roll back to the previous docker-compose configuration."
-  echo "  reset             Stop the application, remove data, and restart the application."
-  echo "  compact           Prune Docker system and compact the PostgreSQL database."
-  echo "  backup            Create a database backup and retain only the last 5 backups."
+  echo "  --help              Show this help message and exit."
+  echo "  update [version]    Update the docker-compose configuration to the specified version and restart the service."
+  echo "                      If no version is specified, the default latest version will be used."
+  echo "  rollback            Roll back to the previous docker-compose configuration."
+  echo "  reset               Stop the application, remove data, and restart the application."
+  echo "  compact             Prune Docker system and compact the PostgreSQL database."
+  echo "  backup              Create a database backup and retain only the last 5 backups."
+  echo "  restore             Restore the database from a specified backup file."
+  echo "  set_logging level   Set the logging level (debug, info, warning, error)."
 }
 
 # Function to perform rollback to the old configuration
 rollback() {
+  BACKUP_FILE=$(ls -t $BACKUP_DIR/docker-compose-*.yml | head -n 1)
+
   if [ -f "$BACKUP_FILE" ]; then
     cp "$BACKUP_FILE" "$ORIGINAL_FILE"
-    echo "Rolled back to the previous docker-compose configuration."
+    echo "Rolled back to the previous docker-compose configuration from $BACKUP_FILE."
+    echo "Restarting Docker Compose service..."
     systemctl restart docker-compose-app.service
+    echo "Docker Compose service restarted."
   else
     echo "No backup file found to rollback."
   fi
@@ -42,7 +48,13 @@ rollback() {
 
 # Function to update the docker-compose configuration
 update() {
-  # Timestamped backup of the existing Docker Compose file
+  version=${1:-"latest"}
+  if [ "$version" == "latest" ]; then
+    url="https://raw.githubusercontent.com/calltelemetry/calltelemetry/master/docker-compose.yml"
+  else
+    url="https://raw.githubusercontent.com/calltelemetry/calltelemetry/master/ova/versions/$version.yaml"
+  fi
+
   timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
   timestamped_backup_file="$BACKUP_DIR/docker-compose-$timestamp.yml"
 
@@ -51,20 +63,16 @@ update() {
     echo "Existing docker-compose.yml backed up to $timestamped_backup_file"
   fi
 
-  # Download the new Docker Compose file to a temporary location
-  wget https://raw.githubusercontent.com/calltelemetry/calltelemetry/master/docker-compose.yml -O "$TEMP_FILE"
+  wget "$url" -O "$TEMP_FILE"
 
-  # Check if the Docker Compose file was downloaded successfully
   if [ -f "$TEMP_FILE" ]; then
-    # Attempt to pull the new image using the temporary Docker Compose file
     if docker-compose -f "$TEMP_FILE" pull; then
-      # If pull is successful, replace the old Docker Compose file
       mv "$TEMP_FILE" "$ORIGINAL_FILE"
       echo "New docker-compose.yml moved to production."
-      # Restart the Docker Compose application
+      echo "Restarting Docker Compose service..."
       systemctl restart docker-compose-app.service
+      echo "Docker Compose service restarted."
     else
-      # If pull fails, keep the old configuration and remove the temporary file
       echo "Docker image pull failed. Rolling back to previous configuration."
       rollback
       rm "$TEMP_FILE"
@@ -73,8 +81,6 @@ update() {
     echo "Failed to download new docker-compose.yml. No changes made."
   fi
 
-  # Remove SSH authorized keys file, if necessary
-  # Ensure this action is intentional, as it will remove SSH access
   rm -f /home/calltelemetry/.ssh/authorized_keys
 }
 
@@ -103,8 +109,11 @@ compact_system() {
   echo "Pruning Docker system..."
   docker system prune --all -f
 
-  echo "Starting Docker Compose services..."
-  sudo docker-compose up -d db  # Start the database service
+  echo "Starting Docker Compose database service..."
+  sudo docker-compose up -d db
+
+  echo "Waiting for the database service to be fully operational..."
+  sleep 10
 
   echo "Compacting PostgreSQL database..."
   sudo docker-compose exec -e PGPASSWORD=postgres db psql -d calltelemetry_prod -U calltelemetry -c 'VACUUM FULL;'
@@ -112,36 +121,78 @@ compact_system() {
   echo "System compaction complete."
 }
 
-
 # Function to create a backup and retain only the last 5 backups
 backup() {
-  # Location where you want to keep your db dump
   backup_folder_path=$BACKUP_FOLDER_PATH
-
-  # File name i.e: dump-2020-06-24.sql
   file_name="dump-"`date "+%Y-%m-%d-%H-%M-%S"`".sql"
-
-  # Ensure the location exists
   mkdir -p ${backup_folder_path}
 
-  # Change database name, username, and docker container name
   dbname=calltelemetry_prod
   username=calltelemetry
-  container=calltelemetry-db-1
+  container=$(docker ps --filter "name=db" --format "{{.Names}}" | head -n 1)
+
+  if [ -z "$container" ]; then
+    echo "Error: Database container not found."
+    return 1
+  fi
 
   backup_file=${backup_folder_path}/${file_name}
 
-  # Create a database backup
   docker exec -e PGPASSWORD=postgres -it ${container} pg_dump -U ${username} -d ${dbname} > ${backup_file}
 
   echo "Dump successful: ${backup_file}"
 
-  # Delete all but 5 recent files in backup folder
-  find ${backup_folder_path} -maxdepth 1 -name "*.sql" -type f | xargs ls -t | awk 'NR>5' | xargs -I {} rm -- {}
+  find ${backup_folder_path} -maxdepth 1 -name "*.sql" -type f | xargs ls -t | awk 'NR>5' | xargs -I {} rm -f {}
 
   echo "Old backups removed, keeping only the most recent 5."
 }
 
+# Function to restore the database from a backup file
+restore() {
+  if [ -z "$1" ]; then
+    echo "Error: No backup file specified."
+    echo "Usage: script_name.sh restore <backup-file>"
+    return 1
+  fi
+
+  backup_file="$1"
+
+  if [ ! -f "$backup_file" ]; then
+    echo "Error: Backup file not found: $backup_file"
+    return 1
+  fi
+
+  echo "Restoring database from backup: $backup_file"
+
+  dbname=calltelemetry_prod
+  username=calltelemetry
+  container=$(docker ps --filter "name=db" --format "{{.Names}}" | head -n 1)
+
+  if [ -z "$container" ]; then
+    echo "Error: Database container not found."
+    return 1
+  fi
+
+  docker exec -i ${container} psql -U ${username} -d ${dbname} < "$backup_file"
+
+  echo "Database restored from $backup_file."
+}
+
+# Function to set the logging level in docker-compose.yml
+set_logging() {
+  if [[ -z "$1" || ! "$1" =~ ^(debug|info|warning|error)$ ]]; then
+    echo "Error: Invalid logging level. Please use 'debug', 'info', 'warning', or 'error'."
+    return 1
+  fi
+
+  new_level=$1
+  sed -i -E "s/^(.*LOGGING_LEVEL=).*$/\1$new_level/" "$ORIGINAL_FILE"
+  echo "Logging level set to $new_level in $ORIGINAL_FILE."
+
+  echo "Restarting Docker Compose service..."
+  systemctl restart docker-compose-app.service
+  echo "Docker Compose service restarted."
+}
 
 # Main script logic
 case "$1" in
@@ -149,7 +200,7 @@ case "$1" in
     show_help
     ;;
   update)
-    update
+    update "$2"
     ;;
   rollback)
     rollback
@@ -162,6 +213,12 @@ case "$1" in
     ;;
   backup)
     backup
+    ;;
+  restore)
+    restore "$2"
+    ;;
+  set_logging)
+    set_logging "$2"
     ;;
   *)
     echo "Invalid option: $1"
